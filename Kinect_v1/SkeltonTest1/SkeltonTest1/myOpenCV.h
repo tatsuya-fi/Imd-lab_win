3f89e33f91b49b4f4af9735ea93c4c4e7c2d6a8b
Tatsuya Fukui <basket_291@yahoo.co.jp>
2014-07-28 19:24:16 +0900

    定期バックアップ

--- Kinect_v1/SkeltonTest1/SkeltonTest1/myOpenCV.h
+++ Kinect_v1/SkeltonTest1/SkeltonTest1/myOpenCV.h
@@ -1,3 +1,11 @@
+#pragma once
+
+/// <TODO>
+/// 追加のインクルードディレクトリに"C:\opencv249\opencv\build\include"を追加
+/// </TODO>
+
+#include <opencv2/opencv.hpp>
+
 // バージョン取得
 #define CV_VERSION_STR CVAUX_STR(CV_MAJOR_VERSION) CVAUX_STR(CV_MINOR_VERSION) CVAUX_STR(CV_SUBMINOR_VERSION)
 
@@ -8,18 +16,57 @@
 #define CV_EXT_STR ".lib"
 #endif
 
-// ライブラリのリンク（不要な物はコメントアウト）
-#pragma comment(lib, "D:\\opencv" CV_VERSION_STR "\\opencv\\build\\x86\\vc12\\lib\\opencv_core"       CV_VERSION_STR CV_EXT_STR)
-#pragma comment(lib, "D:\\opencv" CV_VERSION_STR "\\opencv\\build\\x86\\vc12\\lib\\opencv_highgui"    CV_VERSION_STR CV_EXT_STR)
-#pragma comment(lib, "D:\\opencv" CV_VERSION_STR "\\opencv\\build\\x86\\vc12\\lib\\opencv_imgproc"    CV_VERSION_STR CV_EXT_STR)
-#pragma comment(lib, "D:\\opencv" CV_VERSION_STR "\\opencv\\build\\x86\\vc12\\lib\\opencv_calib3d"    CV_VERSION_STR CV_EXT_STR)
-//#pragma comment(lib, "D:\\opencv" CV_VERSION_STR "\\opencv\\build\\x86\\vc12\\lib\\opencv_gpu"        CV_VERSION_STR CV_EXT_STR)
-//#pragma comment(lib, "D:\\opencv" CV_VERSION_STR "\\opencv\\build\\x86\\vc12\\lib\\opencv_video"      CV_VERSION_STR CV_EXT_STR)
-#pragma comment(lib, "D:\\opencv" CV_VERSION_STR "\\opencv\\build\\x86\\vc12\\lib\\opencv_objdetect"  CV_VERSION_STR CV_EXT_STR)
-//#pragma comment(lib, "D:\\opencv" CV_VERSION_STR "\\opencv\\build\\x86\\vc12\\lib\\opencv_features2d" CV_VERSION_STR CV_EXT_STR)
-//#pragma comment(lib, "D:\\opencv" CV_VERSION_STR "\\opencv\\build\\x86\\vc12\\lib\\opencv_flann"      CV_VERSION_STR CV_EXT_STR)
-//#pragma comment(lib, "D:\\opencv" CV_VERSION_STR "\\opencv\\build\\x86\\vc12\\lib\\opencv_ffmpeg"     CV_VERSION_STR CV_EXT_STR)
-//#pragma comment(lib, "D:\\opencv" CV_VERSION_STR "\\opencv\\build\\x86\\vc12\\lib\\opencv_ts"         CV_VERSION_STR CV_EXT_STR)
-//#pragma comment(lib, "D:\\opencv" CV_VERSION_STR "\\opencv\\build\\x86\\vc12\\lib\\opencv_contrib"    CV_VERSION_STR CV_EXT_STR)
-//#pragma comment(lib, "D:\\opencv" CV_VERSION_STR "\\opencv\\build\\x86\\vc12\\lib\\opencv_ml"         CV_VERSION_STR CV_EXT_STR)
-//#pragma comment(lib, "D:\\opencv" CV_VERSION_STR "\\opencv\\build\\x86\\vc12\\lib\\opencv_legacy"     CV_VERSION_STR CV_EXT_STR)
+/// <Summary>
+/// ライブラリのリンク（不要な物はコメントアウト）
+///　</Summary>
+//　作業PC用
+#pragma comment(lib, "C:\\opencv" CV_VERSION_STR "\\opencv\\build\\x86\\vc11\\lib\\opencv_core"       CV_VERSION_STR CV_EXT_STR)
+#pragma comment(lib, "C:\\opencv" CV_VERSION_STR "\\opencv\\build\\x86\\vc11\\lib\\opencv_highgui"    CV_VERSION_STR CV_EXT_STR)
+#pragma comment(lib, "C:\\opencv" CV_VERSION_STR "\\opencv\\build\\x86\\vc11\\lib\\opencv_imgproc"    CV_VERSION_STR CV_EXT_STR)
+#pragma comment(lib, "C:\\opencv" CV_VERSION_STR "\\opencv\\build\\x86\\vc11\\lib\\opencv_calib3d"    CV_VERSION_STR CV_EXT_STR)
+//#pragma comment(lib, "C:\\opencv" CV_VERSION_STR "\\opencv\\build\\x86\\vc11\\lib\\opencv_gpu"        CV_VERSION_STR CV_EXT_STR)
+//#pragma comment(lib, "C:\\opencv" CV_VERSION_STR "\\opencv\\build\\x86\\vc11\\lib\\opencv_video"      CV_VERSION_STR CV_EXT_STR)
+#pragma comment(lib, "C:\\opencv" CV_VERSION_STR "\\opencv\\build\\x86\\vc11\\lib\\opencv_objdetect"  CV_VERSION_STR CV_EXT_STR)
+//#pragma comment(lib, "C:\\opencv" CV_VERSION_STR "\\opencv\\build\\x86\\vc11\\lib\\opencv_features2d" CV_VERSION_STR CV_EXT_STR)
+//#pragma comment(lib, "C:\\opencv" CV_VERSION_STR "\\opencv\\build\\x86\\vc11\\lib\\opencv_flann"      CV_VERSION_STR CV_EXT_STR)
+//#pragma comment(lib, "C:\\opencv" CV_VERSION_STR "\\opencv\\build\\x86\\vc11\\lib\\opencv_ffmpeg"     CV_VERSION_STR CV_EXT_STR)
+//#pragma comment(lib, "C:\\opencv" CV_VERSION_STR "\\opencv\\build\\x86\\vc11\\lib\\opencv_ts"         CV_VERSION_STR CV_EXT_STR)
+//#pragma comment(lib, "C:\\opencv" CV_VERSION_STR "\\opencv\\build\\x86\\vc11\\lib\\opencv_contrib"    CV_VERSION_STR CV_EXT_STR)
+//#pragma comment(lib, "C:\\opencv" CV_VERSION_STR "\\opencv\\build\\x86\\vc11\\lib\\opencv_ml"         CV_VERSION_STR CV_EXT_STR)
+//#pragma comment(lib, "C:\\opencv" CV_VERSION_STR "\\opencv\\build\\x86\\vc11\\lib\\opencv_legacy"     CV_VERSION_STR CV_EXT_STR)
+// ミーティング室用
+//#pragma comment(lib, "C:\\opencv" CV_VERSION_STR "\\build\\x86\\vc11\\lib\\opencv_core"       CV_VERSION_STR CV_EXT_STR)
+//#pragma comment(lib, "C:\\opencv" CV_VERSION_STR "\\build\\x86\\vc11\\lib\\opencv_highgui"    CV_VERSION_STR CV_EXT_STR)
+//#pragma comment(lib, "C:\\opencv" CV_VERSION_STR "\\build\\x86\\vc11\\lib\\opencv_imgproc"    CV_VERSION_STR CV_EXT_STR)
+//#pragma comment(lib, "C:\\opencv" CV_VERSION_STR "\\build\\x86\\vc11\\lib\\opencv_calib3d"    CV_VERSION_STR CV_EXT_STR)
+////#pragma comment(lib, "C:\\opencv" CV_VERSION_STR "\\build\\x86\\vc11\\lib\\opencv_gpu"        CV_VERSION_STR CV_EXT_STR)
+////#pragma comment(lib, "C:\\opencv" CV_VERSION_STR "\\build\\x86\\vc11\\lib\\opencv_video"      CV_VERSION_STR CV_EXT_STR)
+//#pragma comment(lib, "C:\\opencv" CV_VERSION_STR "\\build\\x86\\vc11\\lib\\opencv_objdetect"  CV_VERSION_STR CV_EXT_STR)
+////#pragma comment(lib, "C:\\opencv" CV_VERSION_STR "\\build\\x86\\vc11\\lib\\opencv_features2d" CV_VERSION_STR CV_EXT_STR)
+////#pragma comment(lib, "C:\\opencv" CV_VERSION_STR "\\build\\x86\\vc11\\lib\\opencv_flann"      CV_VERSION_STR CV_EXT_STR)
+////#pragma comment(lib, "C:\\opencv" CV_VERSION_STR "\\build\\x86\\vc11\\lib\\opencv_ffmpeg"     CV_VERSION_STR CV_EXT_STR)
+////#pragma comment(lib, "C:\\opencv" CV_VERSION_STR "\\build\\x86\\vc11\\lib\\opencv_ts"         CV_VERSION_STR CV_EXT_STR)
+////#pragma comment(lib, "C:\\opencv" CV_VERSION_STR "\\build\\x86\\vc11\\lib\\opencv_contrib"    CV_VERSION_STR CV_EXT_STR)
+////#pragma comment(lib, "C:\\opencv" CV_VERSION_STR "\\build\\x86\\vc11\\lib\\opencv_ml"         CV_VERSION_STR CV_EXT_STR)
+////#pragma comment(lib, "C:\\opencv" CV_VERSION_STR "\\build\\x86\\vc11\\lib\\opencv_legacy"     CV_VERSION_STR CV_EXT_STR)
+
+
+
+
+/////////////////////////////////
+// Additional option
+
+// キー入力
+#define KEY_ESC		0x1b
+#define KEY_SPACE	0x20
+
+
+/// マウスイベント
+//コールバック関数からのイベント情報を取得するための構造体
+typedef struct MouseParam{
+	unsigned int x;
+	unsigned int y;
+	int event;
+	int flags;
+} MouseParam;
+
